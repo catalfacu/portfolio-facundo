145a5c32f2d60df9a6c1d1af13c6e625e6c0cdef
catalfacu <cataldof21@gmail.com>
2023-11-06 17:38:44 -0300

    Creacion de componentes y rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import Landing from "./components/landing/Landing";
+import AboutMe from "./components/About me/AboutMe";
+import Studies from "./components/Studies/Studies";
+import Techs from "./components/Techs/Techs";
+import Contact from "./components/Contact/Contact";
+import NavBar from "./components/navBar/NavBar";
+import {Routes, Route, useLocation} from 'react-router-dom';
+
 
 function App() {
 
+  const location = useLocation();
+
   return (
     <>
-     <h1>HOLAAA!</h1>
+    {
+      location.pathname !== "/" && <NavBar/>
+    }
+    
+    <Routes>
+      <Route path='/' element={<Landing/>}/>
+      <Route path='/aboutme' element={<AboutMe/>}/>
+      <Route path='/studies' element={<Studies/>}/>
+      <Route path='/techs' element={<Techs/>}/>
+      <Route path="/Contact" element={<Contact/>}/>
+    </Routes>
     </>
   )
 }
