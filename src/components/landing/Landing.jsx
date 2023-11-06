da62dffc385e2f92459df04505c2fbd16d9db3d8
catalfacu <cataldof21@gmail.com>
2023-11-06 18:46:04 -0300

    Landing page edicion

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,11 +1,61 @@
 import React from 'react'
+import styles from "./Landing.module.css";
+import { BigHead } from '@bigheads/core'
+import { useNavigate } from 'react-router-dom';
 
 function Landing() {
+    const navigate = useNavigate();
+const handleNavigate = () => {
+    navigate("/aboutme");
+}
+
   return (
-    <div>
-    <h1>Landing</h1>
+    <div className={styles.container}>
+      <div className={styles.texts}>
+        <h1>Facundo Nicolas Cataldo</h1>
+        <h2>
+          Fullstack developer || Javascript || React || Node || Express ||
+          PostgreSQL || Sequelize
+        </h2>
+      </div>
+
+      <div className={styles.avatar}>
+        <BigHead
+          accessory="roundGlasses"
+          body="chest"
+          circleColor="black"
+          clothing="shirt"
+          clothingColor="black"
+          eyebrows="raised"
+          eyes="normal"
+          faceMask={false}
+          faceMaskColor="red"
+          facialHair="none3"
+          graphic="react"
+          hair="buzz"
+          hairColor="black"
+          hat="none"
+          hatColor="green"
+          lashes={false}
+          lipColor="purple"
+          mask
+          mouth="grin"
+          skinTone="brown"
+        />
+      </div>
+
+      <div className={styles.frases}>
+        <p>
+          "No necesitas ser un genio para escribir código,
+          <br /> solo necesitas ser apasionado." <br />- Ralph Johnson
+        </p>
+      </div>
+
+      <div className={styles.button}>
+        <button onClick={handleNavigate}>Comencemos!</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Landing
