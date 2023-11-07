44f5c1f5f8e8e1373f6d3638a5468828a5f7a28a
catalfacu <cataldof21@gmail.com>
2023-11-07 12:13:51 -0300

    creacion de nav bar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import styles from './NavBar.module.css';
+import {NavLink} from 'react-router-dom'; 
+
+
 
 function navBar() {
   return (
-    <div>
-      <h1>Nav</h1>
+    <div className={styles.container}>
+      <button>Menu</button>
+      <nav className={styles.navbar}>
+        <NavLink>Sobre Mi</NavLink>
+        <NavLink>Estudios</NavLink>
+        <NavLink>Tecnologías</NavLink>
+        <NavLink>Contacto</NavLink>
+        <NavLink>Descarga mi CV</NavLink>
+      </nav>
     </div>
   )
 }
