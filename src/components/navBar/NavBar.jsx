a6237e8921e57faa79ec300612976ccac0849fd5
catalfacu <cataldof21@gmail.com>
2023-11-07 13:05:10 -0300

    Enrutamiento de la navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import styles from './NavBar.module.css';
 import {NavLink} from 'react-router-dom'; 
 
-
-
 function navBar() {
   return (
     <div className={styles.container}>
       <button>Menu</button>
       <nav className={styles.navbar}>
-        <NavLink>Sobre Mi</NavLink>
-        <NavLink>Estudios</NavLink>
-        <NavLink>Tecnologías</NavLink>
-        <NavLink>Contacto</NavLink>
-        <NavLink>Descarga mi CV</NavLink>
+        <NavLink to="/aboutme">Sobre mi</NavLink>
+        <NavLink to="/studies">Estudios</NavLink>
+        <NavLink to="/techs">Tecnologías</NavLink>
+        <NavLink to="/contact">Contacto</NavLink>
+        <button>Descarga mi CV</button>
       </nav>
     </div>
   )
