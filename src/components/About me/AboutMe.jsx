182eb4ab9b38b495806b162986949d0fd3267b44
catalfacu <cataldof21@gmail.com>
2023-11-09 16:49:45 -0300

    Estilos y datos de about

--- src/components/About me/AboutMe.jsx
+++ src/components/About me/AboutMe.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles from './AboutMe.module.css'
+import data from '../Helpers/Data';
 
 function AboutMe() {
   return (
-    <div>AboutMe</div>
+    <div className={styles.container}>
+      <h1>About Me</h1>
+      {
+        data
+        ? data.map((el,index)=>{
+          return(
+            <div className={styles.text} key={index}>
+              <span>
+                {el.text}
+              </span>
+            </div>
+          )
+        })
+        : <span>No hay texto</span>
+      }
+    <div className={styles.picture}>
+      <img src="https://res.cloudinary.com/dmgkhl6ys/image/upload/v1693009925/portfolio/Dise%C3%B1o_sin_t%C3%ADtulo_xiqvpx.png" alt="foto-facu" />
+    </div>
+      
+    </div>
   )
 }
 
