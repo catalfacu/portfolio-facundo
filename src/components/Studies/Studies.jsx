78cb48887cc5fe51eb6976426d71bca195401d33
catalfacu <cataldof21@gmail.com>
2023-11-10 18:22:31 -0300

    Componente estudios con informacion lista

--- src/components/Studies/Studies.jsx
+++ src/components/Studies/Studies.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import data from '../Helpers/Data'
+import styles from './Studies.module.css';
 
 function Studies() {
+const studies = data[0].estudios;
+
   return (
-    <div>Studies</div>
+    <div className={styles.container}>
+      <h1>Estudios</h1>
+      {
+        studies
+        ? studies.map((el,index)=>{
+          return(
+            <div key={index} className={styles.cards}>
+              <h2>{el.nombreInstitucion}</h2>
+              <h3>{el.Titulo}</h3>
+              <h4>{el.Duracion}</h4>
+              <p>{el.Descripcion}</p>
+            </div>
+          )
+        })
+        : <span>NO HAY NADA</span>
+      }
+    </div>
   )
 }
 
