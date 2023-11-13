d22137f013faf534083c7cec7a7e476a0e760011
catalfacu <cataldof21@gmail.com>
2023-11-12 21:29:10 -0300

    Ajustes de maquetacion

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,62 +11,60 @@ const socialMedia = data[0].socialMedia;
       <h1 className>Comunicate Conmigo!</h1>
 
       <main className={styles.contact_container}>
-      <div className={styles.form_container}>
-        <form action="submit" className={styles.form}>
+        <div className={styles.form_container}>
+          <form action="submit" className={styles.form}>
+            <section className={styles.field}>
+              <label htmlFor="name">Nombre</label>
+              <input type="text" />
+            </section>
 
-          <section className={styles.field}>
-          <label htmlFor="name">Nombre</label>
-          <input type="text" />
-          </section>
+            <section className={styles.field}>
+              <label htmlFor="email">Email</label>
+              <input type="text" />
+            </section>
 
-          <section className={styles.field}>
-          <label htmlFor="email">Email</label>
-          <input type="text" />
-          </section>
+            <section className={styles.field}>
+              <label htmlFor="message">Mensaje</label>
+              <textarea type="text" />
+            </section>
 
-          <section className={styles.field}>
-          <label htmlFor="message">Mensaje</label>
-          <textarea type="text" />
-          </section>
-
-          <button>Enviar</button>
-        </form>
-      </div>
-
-      <div className={styles.other_forms}>
-        <h2>Mis Redes Sociales</h2>
-        <div className={styles.social_container}>
-          {socialMedia ? (
-            socialMedia.map((el, index) => {
-              return (
-                <article key={index} className={styles.social}>
-                  <a href={el.link}>
-                    <img src={el.img} alt={el.name} />
-                  </a>
-                  <h5>{el.name}</h5>
-                </article>
-              );
-            })
-          ) : (
-            <span>Nada aqui!</span>
-          )}
+            <button>Enviar</button>
+          </form>
         </div>
 
-        <div className={styles.calendar}>
-          <a
-            href="https://calendly.com/cataldof21/entrevista-reunion"
-            target="blank"
-          >
-            <img
-              src="https://res.cloudinary.com/dmgkhl6ys/image/upload/v1693454072/portfolio/g-calendar_gsupfp.png"
-              alt="calendar-logo"
-            />
-          </a>
-          <h5>Agenda Una Reunion!</h5>
+        <div className={styles.other_forms}>
+          <h2>Mis Redes Sociales</h2>
+          <div className={styles.social_container}>
+            {socialMedia ? (
+              socialMedia.map((el, index) => {
+                return (
+                  <article key={index} className={styles.social}>
+                    <a href={el.link}>
+                      <img src={el.img} alt={el.name} />
+                    </a>
+                    <h5>{el.name}</h5>
+                  </article>
+                );
+              })
+            ) : (
+              <span>Nada aqui!</span>
+            )}
+          </div>
+
+          <div className={styles.calendar}>
+            <a
+              href="https://calendly.com/cataldof21/entrevista-reunion"
+              target="blank"
+            >
+              <img
+                src="https://res.cloudinary.com/dmgkhl6ys/image/upload/v1693454072/portfolio/g-calendar_gsupfp.png"
+                alt="calendar-logo"
+              />
+            </a>
+            <h5>Agenda Una Reunion!</h5>
+          </div>
         </div>
-      </div>
       </main>
-
     </div>
   );
 }
