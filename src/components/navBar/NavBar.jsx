38c1c1daac24ae5f9576fb53be3f1df04a0623bc
catalfacu <cataldof21@gmail.com>
2023-11-14 19:07:32 -0300

    Estilos de navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './NavBar.module.css';
-import {NavLink} from 'react-router-dom'; 
+import {NavLink} from 'react-router-dom';
+import {AlignJustify} from 'lucide-react';
+import { Download } from 'lucide-react';
 
 function navBar() {
   return (
     <div className={styles.container}>
-      <button>Menu</button>
+      <button className={styles.button_menu}>
+      <AlignJustify size={30} color="#7741c8" strokeWidth={3} />
+      </button>
       <nav className={styles.navbar}>
         <NavLink to="/aboutme">Sobre mi</NavLink>
         <NavLink to="/studies">Estudios</NavLink>
         <NavLink to="/techs">Tecnologías</NavLink>
         <NavLink to="/projects">Proyectos</NavLink>
         <NavLink to="/contact">Contacto</NavLink>
-        <button>Descarga mi CV</button>
+        <button className={styles.button_menu}>
+          <Download size={35} color='#7741c8' strokeWidth={3}/>
+        </button>
       </nav>
     </div>
   )
