be5e44535a7fb07ecaeec81363e084cbf6c17275
catalfacu <cataldof21@gmail.com>
2023-11-14 18:44:53 -0300

    Estilos del componente contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -35,64 +35,64 @@ const onSubmit = handleSubmit((data,e)=> {
             <section className={styles.field}>
               <label htmlFor="name">Nombre</label>
               <input
-              name="user_name" 
-              type="text"
-              { ...register("user_name", {
-                required: {
-                  value: true,
-                  message: "Su nombre es requerido"
-                },
-                minLength: {
-                  value: 2,
-                  message: "El nombre debe tener mas de 2 caracteres"
-                }
-              })}
+                placeholder="Ingresa tu nombre..."
+                name="user_name"
+                type="text"
+                {...register("user_name", {
+                  required: {
+                    value: true,
+                    message: "Su nombre es requerido",
+                  },
+                  minLength: {
+                    value: 2,
+                    message: "El nombre debe tener mas de 2 caracteres",
+                  },
+                })}
               />
 
-              {
-                errors.user_name && <span>{errors.user_name.message}</span>
-              }
-
+              {errors.user_name && <span>{errors.user_name.message}</span>}
             </section>
 
             <section className={styles.field}>
               <label htmlFor="email">Email</label>
               <input
-              name="email" 
-              type="text"
-              { ...register("email", {
-                required:{
-                  value: true,
-                  message: "Su email es requerido"
-                },
-                pattern: {
-                  value: /^[a-z0-9._%+-]+@[a-z0-9·-]+\.[a-z]{2,4}$/,
-                  message: "Email no valido"
-                }
-              })}
+                placeholder="Ingresa un email..."
+                name="email"
+                type="text"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Su email es requerido",
+                  },
+                  pattern: {
+                    value: /^[a-z0-9._%+-]+@[a-z0-9·-]+\.[a-z]{2,4}$/,
+                    message: "Email no valido",
+                  },
+                })}
               />
-              {
-                errors.email && <span>{errors.email.message}</span>
-              }
+              {errors.email && <span>{errors.email.message}</span>}
             </section>
 
             <section className={styles.field}>
               <label htmlFor="message">Mensaje</label>
-              <textarea name="message" id="message" cols="40" rows="10"
-              { ...register("message", {
-                required: {
-                  value: true,
-                  message: "Un mensaje es necesario"
-                },
-                minLength: {
-                  value: 10,
-                  message: "El mensaje debe tener minimo 10 caracteres"
-                }
-              })}
+              <textarea
+                placeholder='Deja tu mensaje...'
+                name="message"
+                id="message"
+                cols="40"
+                rows="10"
+                {...register("message", {
+                  required: {
+                    value: true,
+                    message: "Un mensaje es necesario",
+                  },
+                  minLength: {
+                    value: 10,
+                    message: "El mensaje debe tener minimo 10 caracteres",
+                  },
+                })}
               />
-              {
-                errors.message && <span>{errors.message.message}</span>
-              }
+              {errors.message && <span>{errors.message.message}</span>}
             </section>
 
             <button>Enviar</button>
@@ -106,7 +106,7 @@ const onSubmit = handleSubmit((data,e)=> {
               socialMedia.map((el, index) => {
                 return (
                   <article key={index} className={styles.social}>
-                    <a href={el.link} target='blank'>
+                    <a href={el.link} target="blank">
                       <img src={el.img} alt={el.name} />
                     </a>
                     <h5>{el.name}</h5>
