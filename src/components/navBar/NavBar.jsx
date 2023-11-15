ed233d20d9881972746aa69ee97b982286d6d5fa
catalfacu <cataldof21@gmail.com>
2023-11-14 21:52:15 -0300

    Iconos y estilos de navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -3,21 +3,36 @@ import styles from './NavBar.module.css';
 import {NavLink} from 'react-router-dom';
 import {AlignJustify} from 'lucide-react';
 import { Download } from 'lucide-react';
+import { User } from 'lucide-react';
+import { LibraryBig } from 'lucide-react';
+import { Wrench } from 'lucide-react';
+import { AppWindow } from 'lucide-react';
+import { MailPlus } from 'lucide-react';
 
 function navBar() {
   return (
     <div className={styles.container}>
       <button className={styles.button_menu}>
-      <AlignJustify size={30} color="#7741c8" strokeWidth={3} />
+      <AlignJustify size={30} color="#7741c8" strokeWidth={2} />
       </button>
       <nav className={styles.navbar}>
-        <NavLink to="/aboutme">Sobre mi</NavLink>
-        <NavLink to="/studies">Estudios</NavLink>
-        <NavLink to="/techs">Tecnologías</NavLink>
-        <NavLink to="/projects">Proyectos</NavLink>
-        <NavLink to="/contact">Contacto</NavLink>
+        <NavLink to="/aboutme">
+          <User size={30} strokeWidth={2}/>
+        </NavLink>
+        <NavLink to="/studies">
+          <LibraryBig size={30} strokeWidth={2}/>
+        </NavLink>
+        <NavLink to="/techs">
+          <Wrench size={30} strokeWidth={2}/>
+        </NavLink>
+        <NavLink to="/projects">
+          <AppWindow size={30} strokeWidth={2}/>
+        </NavLink>
+        <NavLink to="/contact">
+          <MailPlus size={30} strokeWidth={2}/>
+        </NavLink>
         <button className={styles.button_menu}>
-          <Download size={35} color='#7741c8' strokeWidth={3}/>
+          <Download size={35} color='#7741c8' strokeWidth={2}/>
         </button>
       </nav>
     </div>
