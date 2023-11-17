30612f492fd602d13edcd4ae02243809b783cf74
catalfacu <cataldof21@gmail.com>
2023-11-17 19:56:22 -0300

    Estilos del componente estudies

--- src/components/Studies/Studies.jsx
+++ src/components/Studies/Studies.jsx
@@ -8,11 +8,12 @@ const studies = data[0].estudios;
   return (
     <div className={styles.container}>
       <h1>Estudios</h1>
+    <div className={styles.cards}>
       {
         studies
         ? studies.map((el,index)=>{
           return(
-            <div key={index} className={styles.cards}>
+            <div key={index} className={styles.card}>
               <h2>{el.nombreInstitucion}</h2>
               <h3>{el.Titulo}</h3>
               <h4>{el.Duracion}</h4>
@@ -22,6 +23,8 @@ const studies = data[0].estudios;
         })
         : <span>NO HAY NADA</span>
       }
+    </div>
+    
     </div>
   )
 }
