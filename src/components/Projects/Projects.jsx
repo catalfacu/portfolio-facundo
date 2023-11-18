3165e6da62e881c74f76f7af2dba00587beeda66
catalfacu <cataldof21@gmail.com>
2023-11-17 22:07:36 -0300

    Estilos de componente projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,29 +7,31 @@ const projects = data[0].projects;
 
   return (
     <div className={styles.container}>
-        <h1>
-            Mis Proyectos!
-        </h1>
+      <div className={styles.tittle}>
+        <h1>Mis Proyectos!</h1>
+      </div>
 
-        <div className={styles.projects_container}>
-            {
-                projects
-                ? projects.map((el,index)=> {
-                    return(
-                        <article className={styles.project} key={index}>
-                            <div className={styles.project_text}>
-                            <h2>{el.name}</h2>
-                            <p>{el.description}</p>
-                            </div>
-                            <img src={el.img} alt={el.name} />
-                        </article>
-                    )
-                })
-                : <span>No hay projectos!</span>
-            }
-        </div>
+      <div className={styles.projects_container}>
+        {projects ? (
+          projects.map((el, index) => {
+            return (
+              <article className={styles.project} key={index}>
+                <div className={styles.name_photo}>
+                <h2>{el.name}</h2>
+                <img src={el.img} alt={el.name} />
+                </div>
+                <div className={styles.project_description}>
+                  <p>{el.description}</p>
+                </div>
+              </article>
+            );
+          })
+        ) : (
+          <span>No hay projectos!</span>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Projects
