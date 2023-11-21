64b62132465f11ace196d5288b8590f846434959
catalfacu <cataldof21@gmail.com>
2023-11-21 13:02:46 -0300

    Estilos de componente About

--- src/components/About me/AboutMe.jsx
+++ src/components/About me/AboutMe.jsx
@@ -3,28 +3,20 @@ import styles from './AboutMe.module.css'
 import data from '../Helpers/Data';
 
 function AboutMe() {
+
+const text = data[0].text;
   return (
     <div className={styles.container}>
-      <h1>About Me</h1>
-      {
-        data
-        ? data.map((el,index)=>{
-          return(
-            <div className={styles.text} key={index}>
-              <span>
-                {el.text}
-              </span>
-            </div>
-          )
-        })
-        : <span>No hay texto</span>
-      }
-    <div className={styles.picture}>
-      <img src="https://res.cloudinary.com/dmgkhl6ys/image/upload/v1693009925/portfolio/Dise%C3%B1o_sin_t%C3%ADtulo_xiqvpx.png" alt="foto-facu" />
+      <h1>Sobre Mi!</h1>
+      <div className={styles.pictureAndText}>
+        <p className={styles.text}>{text}</p>
+        <img
+          src="https://res.cloudinary.com/dmgkhl6ys/image/upload/v1693009925/portfolio/Dise%C3%B1o_sin_t%C3%ADtulo_xiqvpx.png"
+          alt="foto-facu"
+        />
+      </div>
     </div>
-      
-    </div>
-  )
+  );
 }
 
 export default AboutMe
