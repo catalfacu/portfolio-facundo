e8e95340faff027a9d13df0e3e1737fc773aa2d7
catalfacu <cataldof21@gmail.com>
2024-01-12 19:40:26 -0300

    Eliminando importaciones innecesarias

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Studies from "./components/Studies/Studies";
 import Techs from "./components/Techs/Techs";
 import Contact from "./components/Contact/Contact";
 import NavBar from "./components/navBar/NavBar";
-import {Routes, Route, useLocation} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Projects from "./components/Projects/Projects";
 
 
