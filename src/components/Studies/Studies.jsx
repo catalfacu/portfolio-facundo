d79c3e5b115572192d42bf127854b0cc8119286d
catalfacu <cataldof21@gmail.com>
2024-01-17 17:39:46 -0300

    Cambios en responsive y se añadio el componente timeline a studies

--- src/components/Studies/Studies.jsx
+++ src/components/Studies/Studies.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import data from '../Helpers/Data'
 import styles from './Studies.module.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {Book} from 'lucide-react'
 
 function Studies() {
 const studies = data[0].estudios;
@@ -9,20 +12,30 @@ const studies = data[0].estudios;
     <div className={styles.container}>
       <h1>Estudios</h1>
     <div className={styles.cards}>
-      {
-        studies
-        ? studies.map((el,index)=>{
-          return(
-            <div key={index} className={styles.card}>
-              <h2>{el.nombreInstitucion}</h2>
-              <h3>{el.Titulo}</h3>
-              <h4>{el.Duracion}</h4>
-              <p>{el.Descripcion}</p>
-            </div>
-          )
-        })
-        : <span>NO HAY NADA</span>
-      }
+      <VerticalTimeline
+        lineColor="#D03A3D"
+        layout='1-column-right'
+      >
+        {
+          studies 
+          ? studies.map((study,index)=>{
+            return(
+              <VerticalTimelineElement
+                date={study.Duracion}
+                icon={<Book/>}
+                iconStyle={{background:'#D03A3D'}}
+                className="vertical-timeline-element--education"
+                contentStyle={{ background: '#D03A3D'}}
+              >
+                <h3 className="vertical-timeline-element-title">{study.nombreInstitucion}</h3>
+                <h4 className="vertical-timeline-element-subtitle">{study.Titulo}</h4>
+                <p>{study.Descripcion}</p>
+              </VerticalTimelineElement>
+            )
+          })
+          : <p>Nada</p>
+        }
+      </VerticalTimeline>
     </div>
     
     </div>
